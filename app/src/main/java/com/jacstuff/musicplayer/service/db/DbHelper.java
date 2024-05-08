7a41047d789eb4012b4dc4df608494863e822820
john <johncrawley1@gmail.com>
2024-05-08 23:36:13 +0100

    Removal of redundant DB tables. Also added featured graphic to an assets directory.

--- app/src/main/java/com/jacstuff/musicplayer/service/db/DbHelper.java
+++ app/src/main/java/com/jacstuff/musicplayer/service/db/DbHelper.java
@@ -10,7 +10,7 @@ public class DbHelper extends SQLiteOpenHelper {
 
 
     // If you change the database schema, you must increment the database version.
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
     private static final String DATABASE_NAME = "MusicPlayer.db";
 
     private static final String OPENING_BRACKET = " (";
@@ -22,24 +22,6 @@ public class DbHelper extends SQLiteOpenHelper {
     private static final String CREATE_TABLE_IF_NOT_EXISTS = "CREATE TABLE IF NOT EXISTS ";
 
 
-    private static final String SQL_CREATE_TRACKS_TABLE =
-            CREATE_TABLE_IF_NOT_EXISTS
-                    + DbContract.TracksEntry.TABLE_NAME
-                    + OPENING_BRACKET
-                    + DbContract.TracksEntry._ID + INTEGER + PRIMARY_KEY + COMMA
-                    + DbContract.TracksEntry.COL_TITLE + TEXT + COMMA
-                    + DbContract.TracksEntry.COL_PATH + TEXT + COMMA
-                    + DbContract.TracksEntry.COL_ALBUM + TEXT + COMMA
-                    + DbContract.TracksEntry.COL_ALBUM_ID + TEXT + COMMA
-                    + DbContract.TracksEntry.COL_ARTIST + TEXT + COMMA
-                    + DbContract.TracksEntry.COL_ARTIST_ID + INTEGER + COMMA
-                    + DbContract.TracksEntry.COL_TRACK_NUMBER + INTEGER + COMMA
-                    + DbContract.TracksEntry.COL_DURATION + INTEGER + COMMA
-                    + DbContract.TracksEntry.COL_GENRE + TEXT + COMMA
-                    + DbContract.TracksEntry.COL_DISC + TEXT
-                    + CLOSING_BRACKET;
-
-
     private static final String SQL_CREATE_PLAYLIST_TABLE =
             CREATE_TABLE_IF_NOT_EXISTS
                     + DbContract.PlaylistEntry.TABLE_NAME
@@ -47,27 +29,6 @@ public class DbHelper extends SQLiteOpenHelper {
                     + DbContract.PlaylistEntry._ID + INTEGER + PRIMARY_KEY + COMMA
                     + DbContract.PlaylistEntry.COL_NAME + TEXT
                     + CLOSING_BRACKET;
-
-
-    private static final String SQL_CREATE_ARTISTS_TABLE =
-            CREATE_TABLE_IF_NOT_EXISTS
-                    + DbContract.ArtistsEntry.TABLE_NAME
-                    + OPENING_BRACKET
-                    + DbContract.ArtistsEntry._ID + INTEGER + PRIMARY_KEY + COMMA
-                    + DbContract.ArtistsEntry.COL_NAME + TEXT
-                    + CLOSING_BRACKET;
-
-    private static final String SQL_CREATE_ALBUMS_TABLE =
-            CREATE_TABLE_IF_NOT_EXISTS
-                    + DbContract.AlbumsEntry.TABLE_NAME
-                    + OPENING_BRACKET
-                    + DbContract.AlbumsEntry._ID + INTEGER + PRIMARY_KEY + COMMA
-                    + DbContract.AlbumsEntry.COL_NAME + TEXT
-                    + CLOSING_BRACKET;
-
-    private static final String PLAYLISTS_TABLE_PRIMARY_KEY = DbContract.PlaylistEntry.TABLE_NAME
-            + "."
-            + DbContract.PlaylistEntry._ID;
 
 
     private static final String SQL_CREATE_PLAYLIST_ITEMS_TABLE =
@@ -113,26 +74,8 @@ public class DbHelper extends SQLiteOpenHelper {
 
 
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(SQL_CREATE_TRACKS_TABLE);
         db.execSQL(SQL_CREATE_PLAYLIST_TABLE);
         db.execSQL(SQL_CREATE_PLAYLIST_ITEMS_TABLE);
-        db.execSQL(SQL_CREATE_ARTISTS_TABLE);
-        db.execSQL(SQL_CREATE_ALBUMS_TABLE);
-    }
-
-
-    public void dropAndRecreateTracksArtistsAndAlbumsTables(SQLiteDatabase db){
-        dropTable(DbContract.TracksEntry.TABLE_NAME, db);
-        dropTable(DbContract.ArtistsEntry.TABLE_NAME, db);
-        dropTable(DbContract.AlbumsEntry.TABLE_NAME, db);
-        db.execSQL(SQL_CREATE_TRACKS_TABLE);
-        db.execSQL(SQL_CREATE_ARTISTS_TABLE);
-        db.execSQL(SQL_CREATE_ALBUMS_TABLE);
-    }
-
-
-    private void dropTable(String tableName, SQLiteDatabase db){
-        db.execSQL("DROP TABLE "  + tableName + ";");
     }
 
 
