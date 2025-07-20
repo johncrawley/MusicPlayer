92654c9c5eb4693c7e4b05c8093860f93aceb617
john <johncrawley1@gmail.com>
2025-07-20 10:03:02 +0100

    Amended the Genres Fragment to be a DialogFragment and to use the new layout.

--- app/src/main/java/com/jacstuff/musicplayer/view/fragments/genre/GenresFragment.java
+++ app/src/main/java/com/jacstuff/musicplayer/view/fragments/genre/GenresFragment.java
@@ -1,42 +1,35 @@
 package com.jacstuff.musicplayer.view.fragments.genre;
 
 import static com.jacstuff.musicplayer.view.fragments.FragmentManagerHelper.sendMessages;
-import static com.jacstuff.musicplayer.view.fragments.FragmentManagerHelper.setListener;
 import static com.jacstuff.musicplayer.view.fragments.Message.NOTIFY_TO_DESELECT_ALBUM_ITEMS;
 import static com.jacstuff.musicplayer.view.fragments.Message.NOTIFY_TO_DESELECT_ARTIST_ITEMS;
 import static com.jacstuff.musicplayer.view.fragments.Message.NOTIFY_TO_DESELECT_PLAYLIST_ITEMS;
-import static com.jacstuff.musicplayer.view.fragments.MessageKey.GENRE_UPDATES;
 import static com.jacstuff.musicplayer.view.utils.ListUtils.setVisibilityOnNoItemsFoundText;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.jacstuff.musicplayer.MainActivity;
 import com.jacstuff.musicplayer.R;
-import com.jacstuff.musicplayer.service.ListIndexManager;
-import com.jacstuff.musicplayer.service.MediaPlayerService;
-import com.jacstuff.musicplayer.view.fragments.Message;
 import com.jacstuff.musicplayer.view.fragments.StringListAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class GenresFragment extends Fragment {
+public class GenresFragment extends DialogFragment {
 
     private RecyclerView recyclerView;
-    private StringListAdapter listAdapter;
     private View parentView;
     private TextView noGenresFoundTextView;
-    private ListIndexManager listIndexManager;
-
 
     public GenresFragment() {
         // Required empty public constructor
@@ -45,66 +38,16 @@ public class GenresFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_tab_genres, container, false);
+        return inflater.inflate(R.layout.dialog_load_genre, container, false);
     }
 
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
         this.parentView = view;
-        recyclerView = parentView.findViewById(R.id.genresRecyclerView);
-        noGenresFoundTextView = parentView.findViewById(R.id.noGenresFoundTextView);
+        recyclerView = parentView.findViewById(R.id.recyclerView);
+        noGenresFoundTextView = parentView.findViewById(R.id.noItemsFoundText);
         refreshList();
-        setupFragmentListener();
-        assignListIndexManager();
-        selectSavedIndex();
-    }
-
-
-    private void assignListIndexManager(){
-        MediaPlayerService mediaPlayerService = getMainActivity().getMediaPlayerService();
-        if(mediaPlayerService != null){
-            listIndexManager = mediaPlayerService.getListIndexManager();
-        }
-    }
-
-
-    private void assignIndex(int index){
-        if(listIndexManager == null){
-            assignListIndexManager();
-        }
-        if(listIndexManager != null){
-            listIndexManager.setGenreIndex(index);
-        }
-    }
-
-
-    private void selectSavedIndex(){
-        if(listIndexManager != null){
-            listIndexManager.getGenreIndex().ifPresent(this::scrollToAndSelect);
-        }
-    }
-
-
-    private void scrollToAndSelect(int index){
-        listAdapter.selectItemAt(index);
-        recyclerView.scrollToPosition(index);
-    }
-
-
-    private void setupFragmentListener(){
-        setListener(this, Message.LOAD_GENRES, this::loadGenresToList);
-        setListener(this, Message.NOTIFY_TO_LOAD_GENRE, (bundle) -> listAdapter.selectLongClickItem());
-        setListener(this, Message.NOTIFY_TO_DESELECT_GENRE_ITEMS, (bundle) -> listAdapter.deselectCurrentlySelectedItem());
-    }
-
-
-    private void loadGenresToList(Bundle bundle){
-        ArrayList<String> genreNames =  bundle.getStringArrayList(GENRE_UPDATES.toString());
-        listAdapter.setItems(genreNames);
-        listAdapter.deselectCurrentlySelectedItem();
-        listAdapter.resetSelections();
-        setVisibilityOnNoGenresFoundText(genreNames);
     }
 
 
@@ -114,7 +57,7 @@ public class GenresFragment extends Fragment {
         if(this.parentView == null || genreNames == null){
             return;
         }
-        listAdapter = new StringListAdapter(genreNames, this::loadTracksFromGenre);
+        StringListAdapter listAdapter = new StringListAdapter(genreNames, this::loadTracksFromGenre);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(listAdapter);
@@ -123,9 +66,9 @@ public class GenresFragment extends Fragment {
 
     private void loadTracksFromGenre(String genreName, int position){
         getMainActivity().loadTracksFromGenre(genreName);
-        assignIndex(position);
         notifyOtherFragmentsToDeselectItems();
         toastLoaded();
+        dismissAfterDelay();
     }
 
 
@@ -135,7 +78,6 @@ public class GenresFragment extends Fragment {
                 NOTIFY_TO_DESELECT_ALBUM_ITEMS,
                 NOTIFY_TO_DESELECT_ARTIST_ITEMS);
     }
-
 
 
     private void toastLoaded(){
@@ -152,5 +94,10 @@ public class GenresFragment extends Fragment {
         return (MainActivity)getActivity();
     }
 
+
+
+    private void dismissAfterDelay(){
+        new Handler(Looper.getMainLooper()).postDelayed(this::dismiss, 200);
+    }
 
 }
